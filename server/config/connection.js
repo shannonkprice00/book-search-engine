abf9c0eff874e36347ef715b43a17a89915662b5
Shannon Price <124547956+shannonkprice00@users.noreply.github.com>
2023-07-25 14:16:20 -0500

    attempting to debug deployment issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((error) => console.log('Error connecting to MongoDB: ', error));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;

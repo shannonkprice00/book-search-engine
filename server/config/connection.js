8fcdc639ddd32ab1a18a7d208cc0339cd2033f43
Shannon Price <124547956+shannonkprice00@users.noreply.github.com>
2023-07-25 14:09:08 -0500

    added code to find deployment error

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => console.log('Error connecting to MongoDB: ', error));
 
 module.exports = mongoose.connection;
